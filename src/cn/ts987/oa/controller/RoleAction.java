a686d601ebc42519cbd56cdabe08413d785914e1
fanguangdong <fanguangdong87@gmail.com>
2014-04-21 20:56:28 +0800

    第一次上传 2014.4.21

---
+++ src/cn/ts987/oa/controller/RoleAction.java
@@ -0,0 +1,121 @@
+package cn.ts987.oa.controller;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+
+import cn.ts987.oa.domain.Privilege;
+import cn.ts987.oa.domain.Role;
+
+@Controller
+public class RoleAction extends BaseController<Role>{
+	private static final long serialVersionUID = 1L;
+	
+	private List<Role> roleList;
+	
+	private List<Privilege> topPrivilegeList;
+	
+	private List<Long> privilegeIds;
+	
+	public String setPrivilegeUI() {
+		topPrivilegeList = privilegeService.findTopPrivileges();
+		
+		//long roleId = getModel().getId();
+		long roleId = 1;
+		
+		Role role = roleService.findById(roleId);
+		//getModel().setName(role.getName());
+		
+		if(role.getPrivileges() != null) {
+			privilegeIds = new ArrayList<Long>();
+			for(Privilege p : role.getPrivileges()) {
+				privilegeIds.add(p.getId());
+			}
+		}
+		
+		return "setPrivilegeUI"; 
+	}
+	
+	
+	public String setPrivilege() {
+	
+		//long roleId = getModel().getId();
+		long roleId = 0;
+		Role role = roleService.findById(roleId);
+		
+		if(privilegeIds.size() > 0) {
+			List<Privilege> privilegeList = privilegeService.findByIds(privilegeIds);
+			role.setPrivileges(new HashSet<Privilege>(privilegeList));
+		}
+		roleService.update(role);
+		
+		return "toList";
+	}
+	
+	public String list() throws Exception {
+		roleList = roleService.list();
+		System.out.println("roleList size:  " + roleList.size());
+		//ActionContext.getContext().put("roleList", roleList);
+		return "list";
+		
+	}
+	
+	public String add() throws Exception {
+		Role role = new Role();
+		role.setName(model.getName());
+		role.setDescription(model.getDescription());
+		roleService.add(role);
+		return "toList";
+	}
+	
+	public String update() throws Exception {
+		Role role = roleService.findById(model.getId());
+		role.setName(model.getName());
+		role.setDescription(model.getDescription());
+		roleService.update(role);
+		return "toList";
+	}
+	
+	public String delete() throws Exception {
+		roleService.delete(model.getId());
+		return "toList";
+	}
+	
+	public String addUI() throws Exception {
+		
+		return "saveUI";
+	}
+	
+	public String updateUI() throws Exception {
+		Role role = roleService.findById(model.getId());
+		//ActionContext.getContext().getValueStack().push(role);
+		
+		return "saveUI";
+	}
+	
+	public String toList() throws Exception {
+		
+		return "toList";
+	}
+
+	public void setTopPrivilegeList(List<Privilege> topPrivilegeList) {
+		this.topPrivilegeList = topPrivilegeList;
+	}
+
+	public List<Privilege> getTopPrivilegeList() {
+		return topPrivilegeList;
+	}
+
+	public void setPrivilegeIds(List<Long> privilegeIds) {
+		this.privilegeIds = privilegeIds;
+	}
+
+	public List<Long> getPrivilegeIds() {
+		return privilegeIds;
+	}
+
+	
+
+}
---
+++ src/cn/ts987/oa/interceptor/LoginInterceptor.java
@@ -0,0 +1,44 @@
+package cn.ts987.oa.interceptor;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LoginInterceptor implements HandlerInterceptor{
+
+private Logger logger = Logger.getLogger(LoginInterceptor.class.getName()); 
+	
+	private List<String> excludedUrls;  
+	   
+	public void setExcludedUrls(List<String> excludedUrls) {  
+		this.excludedUrls = excludedUrls;  
+	}   
+	
+	@Override
+	public void afterCompletion(HttpServletRequest arg0,
+			HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
+			Object arg2, ModelAndView arg3) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1,
+			Object arg2) throws Exception {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+}
